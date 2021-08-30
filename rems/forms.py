6d159966aa1f3f7b024580707307808e5dc3c0ef
Nathan Vijay <meganathanv19072@it.ssn.edu.in>
2021-08-30 20:16:02 +0530

    Typo update
    removed typo in forms.py

--- rems/forms.py
+++ rems/forms.py
@@ -7,4 +7,3 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
-g
