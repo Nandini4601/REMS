6bb4d20fcabc7aa608ec32f1cbc43566d1b4a24c
Nathan Vijay <meganathanv19072@it.ssn.edu.in>
2021-09-25 00:35:48 +0530

    Reorganise base.html,updated forms.py

--- rems/forms.py
+++ rems/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DateField
 from wtforms.validators import DataRequired
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from rems.models import Service
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('')
+
+
+class EmployeeAddForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    DoB = DateField(validators=[DataRequired()])
+    mobile = StringField('Mobile Number', validators=[DataRequired()])
+    emer_mobile = StringField('Emergency mobile Number', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
+    service_list = QuerySelectField('Choose service', query_factory=lambda: Service.query, allow_blank=False)
+    gender = StringField('Gender', validators=[DataRequired()])
     submit = SubmitField('')
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from rems import app, db
-from rems.models import User, Employee
+from rems.models import User, Employee,Service,Tenant,House,Apartment
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Employee': Employee}
+    return {'db': db, 'User': User, 'Employee': Employee,'Service': Service, 'Tenant' : Tenant, 'House' : House, 'Apartment' : Apartment}
 
 
 if __name__ == '__main__':
